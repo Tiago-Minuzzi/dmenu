5015b5aa5feb856dce4919ffbdff6ab07be912e3
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-28 19:54:57 -0300

    Catppuccin macchiato theme

---
+++ themes/catppuccin.h
@@ -0,0 +1,7 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cad3f5", "#24273a" },
+	[SchemeSel] = { "#363a4f", "#8aadf4" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
---
+++ themes/default.h
@@ -0,0 +1,7 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#171a1f" },
+	[SchemeSel] = { "#eeeeee", "#4877b1" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
