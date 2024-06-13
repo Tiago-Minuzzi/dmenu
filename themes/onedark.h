40609516aab7f34ce81c55c11f83a53e604f866f
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-06-13 12:54:04 -0300

    Onedark color scheme for dmenu

---
+++ themes/onedark.h
@@ -0,0 +1,7 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#abb2bf", "#252931" },
+	[SchemeSel] = { "#252931", "#528bff" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
